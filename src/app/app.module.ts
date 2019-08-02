a1d9654cd33ce4a9e16b7be43d82f57b49303fe5
Sapna <sapnamgoad1998@gmail.com>
2019-08-02 12:47:49 +0530

    Skeleton Sketched

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonalComponent } from './personal/personal.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
