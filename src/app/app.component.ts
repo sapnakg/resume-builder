50947437e48387661b0c7e8bb7d4935aa8b00dae
Pooja <pooja27kotian@gmail.com>
2019-08-06 14:41:27 +0530

    form updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'resume-builder';
+loadedFeature= 'education';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PersonalComponent } from './personal/personal.component';
 import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { SkillsComponent } from './skills/skills.component';
+//import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import { SkillsComponent } from './skills/skills.component';
     EducationComponent,
     ExperienceComponent,
     SkillsComponent
+    // ,
+    // HeaderComponent
   ],
   imports: [
     BrowserModule,
